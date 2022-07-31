7a027dc0ab7d27835835d3bb4dbec46da6c2a33d
Syeda Raisa Akther <106309369+syedaraisa@users.noreply.github.com>
2022-08-01 00:52:10 +0600

    first commit

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,6 @@
+<?php
+include('../config/constants.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,20 +12,61 @@
 </head>
 <body>
     <div class="login">
-        <div class="admin_logo">
-            <div class="title">
+        <div class="admin_logo"></div>
+        <div class="messege">
+            <?php
+                if(isset($_SESSION['login'])){
+                    echo $_SESSION['login'];
+                    unset($_SESSION['login']);
+                }
+                ?>
+            </div>
                 <form action="" method="post" class="container">
                     <div class="inputbox">
-                        <input type="text" required="required">
+                        <input type="text" name="username" required="required">
                         <span>Username</span>
                     </div>
                     <div class="inputbox">
-                        <input type="password" required="required">
+                        <input type="password" name="password" required="required">
                         <span>Password</span>
+                    </div>
+                    <div>
+                        <button type="submit" name="submit" value="login" class="lg_button">login</button>
                     </div>
                 </form>
             </div>
-        </div>
-    </div>
+        <!-- </div> -->
+    <!-- </div> -->
 </body>
 </html>
+
+<?php
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $password=md5($_POST['password']);
+
+    $sql="SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'";
+    $result=mysqli_query($conn,$sql);
+
+    // Check wheather their have data or not
+    $count=mysqli_num_rows($result);
+    if($count==1){
+    $_SESSION['login']="<div class='success'>Login Successfull.</div>";
+    ?>
+    <script>
+         window.location.href='admin_index.php';
+    </script>
+    <?php
+    exit(0);
+    }else{
+        $_SESSION['login']="Username or password</br>didn't match";
+    ?>
+    <script>
+         window.location.href='login.php';
+    </script>
+    <?php
+    exit(0);
+
+    }
+}
+?>
